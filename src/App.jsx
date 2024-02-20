1258265b37c7a1e729d08a23749a5aa1cdc0ad55
Chniti Yossr <chnitiyossr123@gmail.com>
2024-02-20 15:50:03 +0100

    W3 List in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>hello world</h1>
-      <h1>hello world</h1>
-    </>
-  )
+import * as React from "react";
+function getTitle(title) {
+  return title;
 }
 
-export default App
+const list = [
+  {
+  
+  title: "React",
+  url: "https://reactjs.org/",
+  author: "Jordan Walke",
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: "Redux",
+  url: "https://redux. js.org/",
+  author: "Dan Abramov, Andrew Clark",
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+];
+
+function App() {
+  return (
+  <div>
+  <h1>My Hacker Stories</h1>
+  <label htmlFor="search">Search: </label>
+  <input id="search" type="text" />
+  <hr />
+  <ul>
+
+  {list.map(function (item) {
+  return(
+<li key={item.objectID}>
+<span>
+<a href={item. url}>{item.title}</a>
+</span>
+<span>{item. author}</span>
+<span>{item. num_comments}</span>
+<span>{item. points}</span>
+</li>
+  ); 
+  })}
+  </ul>
+  </div>
+  
+  );
+}
+
+      export default App;
