bfecee9c188607a631b564b32cd554e79b9cc7a3
Chniti Yossr <chnitiyossr123@gmail.com>
2024-02-27 14:43:31 +0100

    W4 React Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -23,31 +23,49 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-  <div>
-  <h1>My Hacker Stories</h1>
-  <label htmlFor="search">Search: </label>
-  <input id="search" type="text" />
-  <hr />
-  <ul>
 
-  {list.map(function (item) {
-  return(
-<li key={item.objectID}>
-<span>
-<a href={item. url}>{item.title}</a>
-</span>
-<span>{item. author}</span>
-<span>{item. num_comments}</span>
-<span>{item. points}</span>
-</li>
-  ); 
-  })}
-  </ul>
-  </div>
+
+      function List() {
+        return (
+        <ul>
+        {list.map(function (item) {
+        return (
+        <li key={item.objectID}>
+        <span>
+        <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        </li>
+        );
+        })}
+        </ul>
+        );
+        }
+
+        function App() {
+          return (
+            <div>
+              <h1>My Hacker Stories</h1>
+              <label htmlFor="search">Search: </label>
+              <input id="search" type="text" />
+              <hr />
+            <List />
+          </div>
+          );
+        }
+          
+        function Search() {
+          return (
+          <div>
+          <label htmlFor="search">Search: </label>
+          <input id="search" type="text" />
+          </div>
+          );
+          } 
+
+        
   
-  );
-}
-
-      export default App;
+          
+          export default App;
