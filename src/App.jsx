1cf94029ce3516288237a5331cba950d9f9e2cdf
Chniti Yossr <chnitiyossr123@gmail.com>
2024-05-16 10:55:48 +0100

    W6 Adding React state for the searchTerm

--- src/App.jsx
+++ src/App.jsx
@@ -84,5 +84,29 @@ const List = (props) => (
     </li>
     
     );
+
+
+    const Search = () => {
+      const [searchTerm, setSearchTerm] = React.useState("');
+    };
+  
+      const Search = () => {
+      const [searchTerm, setSearchTerm] = React.useState(");
+      
+      const handleChange = (event) => {
+      setSearchTerm(event.target.value);
+      };
+      
+      return (
+      <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      
+      <p>
+      Searching for <strong>{searchTerm} </strong>.
+      </p>
+      </div>
+      );
+    };
           
           export default App;
