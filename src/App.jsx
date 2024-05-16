1c7ec24ff1dd48daed22dcc905ce46a27d6fad54
Chniti Yossr <chnitiyossr123@gmail.com>
2024-05-16 11:04:21 +0100

    W6 Filtering the list

--- src/App.jsx
+++ src/App.jsx
@@ -145,4 +145,59 @@ const List = (props) => (
       return ( ... );
       
       };
+
+      const App = () => {
+        const stories = [ ... ];
+        
+        const [searchTerm, setSearchTerm] = React.useState(");
+        
+        const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+        };
+        return (
+        <div>
+        <h1>My Hacker Stories</h1>
+        
+        <Search onSearch={handleSearch} />
+        
+        <hr />
+        
+        <List list={stories} />
+        </div>
+        );
+      };
+        const Search = (props) => (
+        <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={props.onSearch} />
+        </div>
+        );
+
+        const App = () => {
+          const stories = [ ... ];
+          
+          const [searchTerm, setSearchTerm] = React.useState('');
+          
+          const handleSearch = (event) => {
+          setSearchTerm(event.target.value);
+          };
+          const searchedStories = stories. filter(function (story) {
+          return story. title. includes(searchTerm);
+          });
+          
+          return (
+          <div>
+          
+          <h1>My Hacker Stories</h1>
+          
+          <Search onSearch={handleSearch} />
+          
+          <hr />
+          
+          <List list={searchedStories} />
+          </div>
+          );
+        };
+        
+
           export default App;
