9fdbe452d7c39453890d7a833e2b19e7e3ed7888
Chniti Yossr <chnitiyossr123@gmail.com>
2024-05-16 10:58:58 +0100

    W6 Adding callback handler

--- src/App.jsx
+++ src/App.jsx
@@ -109,4 +109,40 @@ const List = (props) => (
       );
     };
           
+    const App = () => {
+      const stories = [ ... ];
+      
+      // A
+      const handleSearch = (event) => {
+      // C
+      console.log(event.target.value);
+      };
+      return (
+      <div>
+      <h1>My Hacker Stories</h1>
+      
+      {/* // B */}
+      <Search onSearch={handleSearch} />
+      
+      <hr />
+      
+      <List list={stories} />
+      </div>
+      
+      );
+    };
+
+    const Search = (props) => {
+      const [searchTerm, setSearchTerm] = React.useState("');
+      
+      const handleChange = (event) => {
+      setSearchTerm(event.target.value);
+      
+      // B
+      props.onSearch(event);
+      };
+      
+      return ( ... );
+      
+      };
           export default App;
