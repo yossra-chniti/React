cbb69ef0e874f4f1b417f006200f69c6496b2398
Chniti Yossr <chnitiyossr123@gmail.com>
2024-05-19 16:59:17 +0100

    W7: props destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,35 @@ const App = () => {
   
   );
 };
-  const Search = (props) => (
+const Search = ({ search, onSearch }) => (
   <div>
   <label htmlFor="search">Search: </label>
   <input
-  
   id="search"
   type="text"
-  value={props.search}
-  onChange={props.onSearch}
+  value={search}
+  onChange={onSearch}
   />
   </div>
-  );
+
+  const List = ({ list }) => (
+    <ul>
+    {list.map((item) => (
+    <Item key={item.objectID} item={item} />
+    ))}
+    </ul>
+    
+    );
+    
+    const Item = ({ item }) => (
+    <li>
+    
+    <span>
+    <a href={item.url}>{item.title} </a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments} </span>
+    <span>{item.points}</span>
+    </li>
+    );
 export default App;
