679d663dc790d5e973c5c9300e7698c045dd8294
Chniti Yossr <chnitiyossr123@gmail.com>
2024-05-19 17:18:22 +0100

    W9:React data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,47 @@
 import * as React from "react";
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+React.useEffect(() => {
+  fetch(/*Define URL resource*/)
+  .then((response) => {
+  //Handle response
+  })
+  .catch(() => {
+  //Handle Error
+  });
+  }, [searchTerm]);
 
-const App = () => {
-  ...
-  return (
-  <div>
-  <h1>My Hacker Stories</h1>
-  
-  <InputWithLabel
-  id="search"
-  label="Search"
-  value={searchTerm}
-  onInputChange={handleSearch}
-  >
-search:
-</InputWithLabel>
-  </div>
-  
-  );
-  };
-  
-  const InputWithLabel = ({
-  id,
-  label,
-  value,
-  type= 'text',
-  onInputChange,
-  children,
-  }) => (
-  <>
-  <label htmlFor={id}>{children}</label>
-  &nbsp;
-  <input
-  id={id}
-  type="type"
-  value={value}
-  onChange={onInputChange}
-  />
-  </>
-  );
-  const [stories, setStories] = React.useState(initialStories);
-  const App = () => {
+  React.useEffect(() => {
+    if (!searchTerm) return;
+   fetch(`${API_ENDPOINT}${searchTerm}`)
+   .then((response) => {
+   //Handle response
+   })
+   .catch(() => {
+   //Handle Error
+   });
+   }, [searchTerm]);
 
-    const [stories, setStories] = React.useState(initialStories);
-    const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-    (story) => item.objectID !== story.objectID
-    );
-    setStories(newStories);
-    };
-    ..
-    return
-    <div>
-    <h1>My Hacker Stories</h1>
-    
-    <hr />
-    <List list={searchedStories} onRemoveItem={handleRemoveStory} />
-    </div>
-    );
-    };
+   React.useEffect(() => {
+    if (!searchTerm) return;
+   fetch(`${API_ENDPOINT}${searchTerm}`)
+   .then((response) => response.json())
+   .catch(() => {
+   //Handle Error
+   });
+   }, [searchTerm]);
 
-    const List = ({ list, onRemoveItem }) => (
-      <ul>
-      {list.map((item) => (
-      <Item
-      key={item.objectID}
-      item={item}
-      onRemoveItem={onRemoveItem}
-      />
-      ))}
-      </ul>
-      );
+   React.useEffect(() => {
+    if (!searchTerm) return;
+   fetch(`${API_ENDPOINT}${searchTerm}`)
+   .then((response) => response.json())
+   .then((result) => {
+   setStories(result.hits);
+   })
+   .catch(() => {
+   //Handle Error
+   });
+   }, [searchTerm]);
 
-const Item = ({ item, onRemoveItem }) => (
-<li>
-<span>
-<a href={item.url}>{item.title}</a>
-</span>
-<span>{item.author} </span>
-<span>{item.num_comments} </span>
-<span> {item.points} </span>
-<span>
-<button type="button" onClick={() => onRemoveItem(item)}>
-Dismiss
-</button>
-</span>
-</li>
+   <List list={stories} onRemoveItem={handleRemoveStory} />
 
-);
 export default App;
