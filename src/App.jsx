06421bdfa9a2af9d6fa3438a303753acc7ec3e84
Chniti Yossr <chnitiyossr123@gmail.com>
2024-05-19 17:14:33 +0100

    W8:React inline handler

--- src/App.jsx
+++ src/App.jsx
@@ -38,4 +38,53 @@ search:
   />
   </>
   );
+  const [stories, setStories] = React.useState(initialStories);
+  const App = () => {
+
+    const [stories, setStories] = React.useState(initialStories);
+    const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+    (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+    };
+    ..
+    return
+    <div>
+    <h1>My Hacker Stories</h1>
+    
+    <hr />
+    <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+    </div>
+    );
+    };
+
+    const List = ({ list, onRemoveItem }) => (
+      <ul>
+      {list.map((item) => (
+      <Item
+      key={item.objectID}
+      item={item}
+      onRemoveItem={onRemoveItem}
+      />
+      ))}
+      </ul>
+      );
+
+const Item = ({ item, onRemoveItem }) => (
+<li>
+<span>
+<a href={item.url}>{item.title}</a>
+</span>
+<span>{item.author} </span>
+<span>{item.num_comments} </span>
+<span> {item.points} </span>
+<span>
+<button type="button" onClick={() => onRemoveItem(item)}>
+Dismiss
+</button>
+</span>
+</li>
+
+);
 export default App;
